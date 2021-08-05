a8776c5158efb455d5c9c2a4f4b4b1994d938bce
developerriaz <59824173+developerriaz@users.noreply.github.com>
2021-08-05 16:22:17 +0200

    First Commit

---
+++ Messaging.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace WongaSender
+{
+    class Messaging
+    {
+        public Messaging(String senderName)
+        {
+            var factory = new ConnectionFactory
+            {
+            //    Uri = new System.Uri("")
+            };
+            
+            //using  var  f = factory.CreateConnection();
+        }
+    }
+}
